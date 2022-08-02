c089ce8e678692dd330ce391569581106d2d739d
ChangMinPark2 <98qkrckdals@naver.com>
2022-08-02 20:04:10 +0900

    controller update

--- src/main/java/hello2/hellospring2/controller/HomeController.java
+++ src/main/java/hello2/hellospring2/controller/HomeController.java
@@ -1,2 +1,14 @@
-package hello2.hellospring2.controller;public class HomeController {
+package hello2.hellospring2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+
+    public String home(){
+        return "home";
+    }
 }
--- src/main/java/hello2/hellospring2/controller/MemberController.java
+++ src/main/java/hello2/hellospring2/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello2.hellospring2.controller;
 import hello2.hellospring2.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +13,9 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/creatMemberForm";
     }
 }
