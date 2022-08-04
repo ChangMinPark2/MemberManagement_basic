e6ce5ef44192bfb00808653612f8a4529e9eefef
ChangMinPark2 <98qkrckdals@naver.com>
2022-08-04 18:45:01 +0900

    lasthtml commit

--- src/main/java/hello2/hellospring2/controller/MemberController.java
+++ src/main/java/hello2/hellospring2/controller/MemberController.java
@@ -4,11 +4,15 @@ import hello2.hellospring2.domain.Member;
 import hello2.hellospring2.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class MemberController {
+
 
     private final MemberService memberService;
 
@@ -32,5 +36,12 @@ public class MemberController {
 
         return "redirect:/"; //회원가입이 끝나면 홈 화면으로 보내기 ?
 
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers(); //멤버를 다 끄집어 온다
+        model.addAttribute("members", members); // 모델에다가 담아서 화면에 넘긴다
+        return "members/memberList";
     }
 }
