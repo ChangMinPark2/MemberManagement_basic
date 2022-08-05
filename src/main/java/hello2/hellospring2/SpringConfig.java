37912f457dc39f01dfac07eecaa5ad2fd72a0ef5
ChangMinPark2 <98qkrckdals@naver.com>
2022-08-05 20:41:47 +0900

    jdbc commit

--- src/main/java/hello2/hellospring2/SpringConfig.java
+++ src/main/java/hello2/hellospring2/SpringConfig.java
@@ -3,11 +3,15 @@ package hello2.hellospring2;
 import hello2.hellospring2.repository.MemberRepository;
 import hello2.hellospring2.repository.MemoryMemberRepository;
 import hello2.hellospring2.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
 
     @Bean
     public MemberService memberService() {
--- src/main/java/hello2/hellospring2/repository/JdbcMemberRepository.java
+++ src/main/java/hello2/hellospring2/repository/JdbcMemberRepository.java
@@ -1,4 +1,29 @@
 package hello2.hellospring2.repository;
 
-public class JdbcMemberRepository {
+import hello2.hellospring2.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcMemberRepository implements MemberRepository{
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
 }
