34d870bcd5fa473630ba84ef2f9b8197ee83c018
ChangMinPark2 <98qkrckdals@naver.com>
2022-08-13 16:23:09 +0900

    SPringDataJpa commit

---
+++ src/main/java/hello2/hellospring2/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,13 @@
+package hello2.hellospring2.repository;
+
+import hello2.hellospring2.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    //JPOL select m from member m where m.name = ?
+    @Override
+    Optional<Member> findByName(String name);
+}
